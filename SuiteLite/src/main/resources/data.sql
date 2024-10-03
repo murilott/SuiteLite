79667154fca002aa47409ae7ac40793b93f7e98d
murilott <murilo_teichert@hotmail.com>
2024-10-02 23:42:54 -0300

    sql

--- SuiteLite/src/main/resources/data.sql
+++ SuiteLite/src/main/resources/data.sql
@@ -1,3 +1,11 @@
--- CREATE DATABASE dbsuite;
-
--- INSERT INTO Album
+SELECT * FROM artista
+SELECT * FROM ouvinte
+SELECT * FROM pessoa
+SELECT * FROM album
+SELECT * FROM musica
+SELECT * FROM lista
+SELECT * FROM pessoa
+INSERT INTO musica VALUES (120, FALSE, 'musica', 100, 1)
+INSERT INTO ouvinte VALUES (1, NULL, 'nome@dominio.com', 'sergio', 'senha123', FALSE)
+INSERT INTO album VALUES (1, 1, 10, 'album1', 2)
+INSERT INTO pessoa VALUES (1, 1, 'nome@dominio.com', 'sergio', 'senha123', FALSE, 10, 1, 1)
